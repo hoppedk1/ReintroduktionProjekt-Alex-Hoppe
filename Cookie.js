cb9ef144c47622728d3624a368f668e54343b866
hoppedk1 <mathiashoppedk@gmail.com>
2021-09-24 08:53:23 +0200

    Kode
    Start skrivning
    Får inspiration fra hvordan koden skal skrives herfra
    https://codepen.io/marisdu/pen/ZJKzRG

---
+++ Cookie.js
@@ -0,0 +1,6 @@
+var num = 0; // Ens starter værdi
+
+function cookieClick(){ // her er coden/funktionen som skal afspilles hver gang der bliver trykket på cookien.
+    num += 1 // Værdien af points man får per click.
+    return ("You clicked")
+}
