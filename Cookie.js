3372a820758852a0bd97f6cb2b42bb82f74abf71
hoppedk1 <mathiashoppedk@gmail.com>
2021-09-24 09:55:52 +0200

    Hijacked code
    Code fra linked, Som jeg gerne vil ændre i

--- Cookie.js
+++ Cookie.js
@@ -1,6 +1,20 @@
 var num = 0; // Ens starter værdi
 
+var cookie = document.getElementById("cookie");
+
+window.onload = function () { // er en del af koden som tvinger en til at skrive noget når spillet loader
+    var name = prompt("What is your name"); // spør om navnet på din buisness
+    var Buisness = prompt("What type of buisness are you running?") // spørger om hvilken type buisness det er, ville gerne have billdet ville ændre sig
+    var space = document.getElementById("space"); // kalder "space" fra HTML hvor at navnet skal stå
+    
+    space.innerHTML = name + "'s" + " " +Buisness; // Ligger combinationen af navne sammen.
+}
+
 function cookieClick(){ // her er coden/funktionen som skal afspilles hver gang der bliver trykket på cookien.
-    num += 1 // Værdien af points man får per click.
-    return ("You clicked")
+    num += 1; // Værdien af points man får per click.
+    console.log("You Clicked");
+
+   var numbers = document.getElementById("numbers");
+
+    numbers.innerHTML = num; // Det er denne del som gør at man kan se selve tallet på HTML filen.
 }
