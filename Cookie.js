85b2dba93de72a7a3e27a4b9f4a58f03394a182f
hoppedk1 <mathiashoppedk@gmail.com>
2021-10-11 12:37:28 +0200

    The mighty CSS fix
    The mighty CSS fix

--- Cookie.js
+++ Cookie.js
@@ -4,15 +4,18 @@ var level = 1; // Start click værdi
 var StrongerClickerPrice = 50; //Her sætter vi prisen
 var BoosterPrice = 5000;
 var SpecialPowderPrice = 25000;
-
 var cookie = document.getElementById("cookie");
-
 window.onload = function () { // er en del af koden som tvinger en til at skrive noget når spillet loader
     var name = prompt("What is your name"); // spør om navnet på din buisness
     var Buisness = prompt("What type of buisness are you running?") // spørger om hvilken type buisness det er, ville gerne have billdet ville ændre sig/ standard her ville nok være et bakery.
     var space = document.getElementById("space"); // kalder "space" fra HTML hvor at navnet skal stå
-    
     space.innerHTML = name + "'s" + " " +Buisness; // Ligger combinationen af navne sammen.
+    ClickIncrease1.innerHTML = 1
+    ClickIncrease2.innerHTML = 50   
+    ClickIncrease3.innerHTML = 500
+    LevelIncrease1.innerHTML = StrongerClickerPrice
+    LevelIncrease2.innerHTML = BoosterPrice
+    LevelIncrease3.innerHTML = SpecialPowderPrice
 }
 
 function cookieClick(){ // her er coden/funktionen som skal afspilles hver gang der bliver trykket på cookien.
@@ -32,6 +35,7 @@ function Upgrade(){
        numbers.innerHTML = num; // ændre selve html delen til at vise du har fået fjernet 50
        StrongerClickerPrice = StrongerClickerPrice*1.1;
        upgradeLevel.innerHTML = level; // denne del af koden viser hvor mange points man får pr click
+       LevelIncrease1.innerHTML = StrongerClickerPrice
        alert("You have upgraded");
     }
     else {
@@ -47,6 +51,7 @@ function UpgradeTwo(){
        upgradeLevel.innerHTML = level ; // Denne del i koden viser hvor mange points man får per click
        numbers.innerHTML = num; // ændre selve html delen til at vise du har fået fjernet 500
        BoosterPrice = BoosterPrice*1.1;
+       LevelIncrease2.innerHTML = BoosterPrice
        alert("You have upgraded 50 points") 
     }
     else {
@@ -62,6 +67,7 @@ function UpgradeThree(){
         upgradeLevel.innerHTML = level; // adder 500 til den del af koden som viser points pr click
         numbers.innerHTML = num; // går at du kan se du har fået fjernet 25k
         SpecialPowderPrice = SpecialPowderPrice*1.1;
+        LevelIncrease3.innerHTML = SpecialPowderPrice
         alert("du har nu klaret spillet")
     }
     else {
