b0192fa4bebe303c3696498cefd092e2cf1b73dd
hoppedk1 <mathiashoppedk@gmail.com>
2021-10-11 11:15:52 +0200

    bnbasbmbmn
    ,jhaskjhads

--- Cookie.js
+++ Cookie.js
@@ -1,6 +1,6 @@
 // Det meste er koden er blevet lånt fra https://codepen.io/marisdu/pen/ZJKzRG
 var num = 0; // Ens starter værdi
-var level = 0;
+var level = 1;
 var cookie = document.getElementById("cookie");
 
 window.onload = function () { // er en del af koden som tvinger en til at skrive noget når spillet loader
@@ -12,7 +12,7 @@ window.onload = function () { // er en del af koden som tvinger en til at skrive
 }
 
 function cookieClick(){ // her er coden/funktionen som skal afspilles hver gang der bliver trykket på cookien.
-    num += 1 + level; // Værdien af points man får per click.
+    num += level; // Værdien af points man får per click.
     console.log("You Clicked");
 
    var numbers = document.getElementById("numbers");
@@ -21,12 +21,12 @@ function cookieClick(){ // her er coden/funktionen som skal afspilles hver gang 
 }
 
 function Upgrade(){
-    if (num > 49){// Her tjekker den om du har over 49 points
+    if (num >= 50){// Her tjekker den om du har over 49 points
         console.log("Du har købt en upgrade"); // besked at du har købt en upgrade måske lav den om til at være en promt
        level += 1; // plus i level så man få flere points pr click
        num -= 50; // fjerner 50 fra din points,
        numbers.innerHTML = num; // ændre selve html delen til at vise du har fået fjernet 50
-       upgradeLevel.innerHTML = level + 1; // denne del af koden viser hvor mange points man får pr click
+       upgradeLevel.innerHTML = level; // denne del af koden viser hvor mange points man får pr click
        alert("You have upgraded");
     }
     else {
@@ -35,11 +35,11 @@ function Upgrade(){
 }
 
 function UpgradeTwo(){
-    if (num > 4999){// Her tjekker den om du har over 999 points
+    if (num >= 5000){// Her tjekker den om du har over 999 points
         console.log("Du har købt en stor upgrade"); // besked at du har købt en upgrade måske lav den om til at være en promt
-        upgradeLevel.innerHTML = level + 50; // Denne del i koden viser hvor mange points man får per click
         level += 50; // plus i level så man få flere points pr click
        num -= 5000; // fjerner 500 fra din points,
+       upgradeLevel.innerHTML = level ; // Denne del i koden viser hvor mange points man får per click
        numbers.innerHTML = num; // ændre selve html delen til at vise du har fået fjernet 500
        alert("You have upgraded 50 points") 
     }
@@ -49,11 +49,11 @@ function UpgradeTwo(){
 }
 
 function UpgradeThree(){
-    if (num > 24999){ // tjek om 25k
+    if (num >= 25000){ // tjek om 25k
         console.log("du har købt den sidste upgrade");
-        upgradeLevel.innerHTML = level + 500; // adder 500 til den del af koden som viser points pr click
         level += 500 // viser point i click 
         num -= 25000 // fjerner dine points
+        upgradeLevel.innerHTML = level + 500; // adder 500 til den del af koden som viser points pr click
         numbers.innerHTML = num; // går at du kan se du har fået fjernet 25k
         alert("du har nu klaret spillet")
     }
