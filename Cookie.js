10becbc69c93ae8eb0543fc3ffe2cdbf901b3712
pascal2203 <75846575+pascal2203@users.noreply.github.com>
2021-10-11 12:11:31 +0200

    Bla

--- Cookie.js
+++ Cookie.js
@@ -2,9 +2,8 @@
 var num = 0; // Ens starter værdi
 var level = 1; // Start click værdi
 var StrongerClickerPrice = 50; //Her sætter vi prisen
-var BoosterPrice = 5000;
-var SpecialPowderPrice = 25000;
-
+var BoosterPrice = 5000;//Her sætter vi prisen
+var SpecialPowderPrice = 25000;//Her sætter vi prisen
 var cookie = document.getElementById("cookie");
 
 window.onload = function () { // er en del af koden som tvinger en til at skrive noget når spillet loader
@@ -28,9 +27,9 @@ function Upgrade(){
     if (num >= StrongerClickerPrice){// Her tjekker den om du har over 49 points
         console.log("Du har købt en upgrade"); // besked at du har købt en upgrade måske lav den om til at være en promt
        level += 1; // plus i level så man få flere points pr click
-       num -= 50; // fjerner 50 fra din points,
+       num -= StrongerClickerPrice; // fjerner 50 fra din points,
        numbers.innerHTML = num; // ændre selve html delen til at vise du har fået fjernet 50
-       StrongerClickerPrice = StrongerClickerPrice*1.1;
+       StrongerClickerPrice = StrongerClickerPrice*1.1; // Her øger vi prisen for hvert køb
        upgradeLevel.innerHTML = level; // denne del af koden viser hvor mange points man får pr click
        alert("You have upgraded");
     }
@@ -43,10 +42,10 @@ function UpgradeTwo(){
     if (num >= BoosterPrice){// Her tjekker den om du har over 999 points
         console.log("Du har købt en stor upgrade"); // besked at du har købt en upgrade måske lav den om til at være en promt
         level += 50; // plus i level så man få flere points pr click
-       num -= 5000; // fjerner 500 fra din points,
+       num -= BoosterPrice; // fjerner 500 fra din points,
        upgradeLevel.innerHTML = level ; // Denne del i koden viser hvor mange points man får per click
        numbers.innerHTML = num; // ændre selve html delen til at vise du har fået fjernet 500
-       BoosterPrice = BoosterPrice*1.1;
+       BoosterPrice = BoosterPrice*1.1;// Her øger vi prisen for hvert køb
        alert("You have upgraded 50 points") 
     }
     else {
@@ -58,10 +57,10 @@ function UpgradeThree(){
     if (num >= SpecialPowderPrice){ // tjek om 25k
         console.log("du har købt den sidste upgrade");
         level += 500 // viser point i click 
-        num -= 25000 // fjerner dine points
+        num -= SpecialPowderPrice // fjerner dine points
         upgradeLevel.innerHTML = level; // adder 500 til den del af koden som viser points pr click
         numbers.innerHTML = num; // går at du kan se du har fået fjernet 25k
-        SpecialPowderPrice = SpecialPowderPrice*1.1;
+        SpecialPowderPrice = SpecialPowderPrice*1.1;// Her øger vi prisen for hvert køb
         alert("du har nu klaret spillet")
     }
     else {
